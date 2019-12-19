480e10307979067bab296b96b360eea292b1ec9f
Onys56 <ony.prochy@seznam.cz>
2019-12-19 02:41:39 +0100

    Actually working version

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Node:
     
     def kill(self):
         """Deletes the node"""
-        self.parent.children.pop()
+        self.parent.children.remove(self)
         del self
 
     def print(self,j=0):
@@ -28,33 +28,40 @@ class Node:
 
 head = Node()
 
+#DEBUG 8/5p2/5P1p/5PkN/6P1/4N1Rp/7P/6KQ w - - 0 2
+#AND
+#board.push(chess.Move.from_uci('a8h1'))
+#board.push(chess.Move.from_uci('h4g5'))
+#Q7/5p2/5P1p/5PPN/6Pk/4N1Rp/7P/6K1 w - - 0 1
+
 def findCheckmates(depth, node):
     moves = board.legal_moves
     returnVal = False
-    if depth > 0 and moves:
+    if moves:
         for myMove in moves:
             board.push(myMove)
             if board.is_checkmate():
                 Node(node, myMove.uci())
                 board.pop()
                 returnVal = True
-            elif depth > 1:
+            elif depth > 1 and board.legal_moves:
                 x = Node(node, myMove.uci())
-                for enemyMove in board.legal_moves:
+                canCheckmate = True
+                for enemyMove in board.legal_moves:                        
                     board.push(enemyMove)
                     y = Node(x, enemyMove.uci())
                     if findCheckmates(depth - 1, y):
                         board.pop()
                     else:
+                        canCheckmate = False
                         x.kill()
                         board.pop()
                         break
+                if canCheckmate:
+                    returnVal = True
                 board.pop()
             else:
                 board.pop()
-    else:
-        returnVal = False
-        board.pop()
     return returnVal
 
 
