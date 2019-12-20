be27059042c78ce1b8f8d73dfe8be0864a56061d
Onys56 <ony.prochy@seznam.cz>
2019-12-20 02:36:00 +0100

    Options and readme

--- README.md
+++ README.md
@@ -1,8 +1,40 @@
 # Check4mate
-Checkmate search for a selected depth (default 2). 
+Checkmate search for a selected depth. 
 
-For a given chess position, finds if it's possible to make guaranteed checkmate. For checkmates in 2 moves or lower, the program prints the table of moves to perform the checkmate based on opponent's move.
+For a given chess position, finds if it's possible to make guaranteed checkmate. The program prints the tree of moves to perform the checkmate based on opponent's moves.
 
-Using libraries: [python-chess](https://python-chess.readthedocs.io/en/latest/index.html)
+# Requirements
+Using libraries: 
+*  [python-chess](https://python-chess.readthedocs.io/en/latest/index.html)
+
 # Usage
-Start main.py in terminal and follow instructions.
+Start main.py in terminal and follow instructions in terminal.
+1. Paste in your FEN
+
+   [FEN](https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation) example: '5bk1/6p1/5PQ1/pp4Pp/2p4P/P2r4/1PK5/8 w - - 1 0'  
+   Only the first part (board state) is required.  
+   The second part (color to play) defaults to white.  
+   If inputted, other parts are accounted for in the algorithm. Defaults to - - 1 0.  
+
+2. Search depth
+
+   Maximum number of fullmoves to reach the checkmate.  
+   Recommended maximal depth: 3 generally takes 5s-60s on average PC.  
+   (For better performance check out the experimental multi-thread branch TODO)
+
+# Options
+Edit options.ini to change
+* Verbose
+
+  Progress prints
+  * Enable
+
+  Should the program print progress: 1 or 0
+  * Depth  
+
+   How many fullmoves down should the program print:  >= 0
+
+   * Time
+
+   Should the program print the time it took to evaluate moves: 1 or 0
+
--- main.py
+++ main.py
@@ -1,47 +1,46 @@
 import chess
 import time
+import configparser
 
-board = chess.Board(input("Enter FEN code: "))
-d = int(input("Enter depth: "))
-color = board.turn #WHITE == True; BLACK = False
-verbDepth = 1
+opt = configparser.ConfigParser()
+opt.read('options.ini')
 
 class Node:
-    def __init__(self, parent=None, move=None):
+    """"Nodes for the tree that represents found checkmates"""
+    def __init__(self, parent=None, move='Checkmates:'):
         self.parent = parent
         self.move = move
         self.children = []
         if parent:  parent.children.append(self)
     
     def kill(self):
-        """Deletes the node"""
+        """Removes the node from its parent and deletes itself"""
         self.parent.children.remove(self)
         del self
 
-    def print(self,j=0):
-        k = j*' '
-        print(k,j,': ',self.move,sep='')
+    def print(self,j=-1, first=True):
+        """Prints the possible checkmates when called on the head"""
+        if first: 
+            if len(self.children) > 0:
+                print('Checkmates:')
+            else:
+                print('No checkmates found')
+        else:
+            k = j*' '
+            t = j // 2 + 1
+            c = 'w' if color != (j%2 == 1) else 'b' 
+            print(f'{k}{t}{c}: {self.move}')
         ch = self.children
         if ch:
             for i in ch:
-                #print(k, end='',sep='')
-                i.print(j+1)
+                i.print(j+1, False)
         
-
-head = Node()
 
 def findCheckmates(depth, node):
     moves = board.legal_moves
     returnVal = False
     if moves:
-        verb = verbDepth > (d-depth)
-        if verb:
-            t = time.time()
-            c = board.legal_moves.count()
-            k = 1
         for myMove in moves:
-            if verb:
-                t = time.time()
             board.push(myMove)
             if board.is_checkmate():
                 Node(node, myMove.uci())
@@ -50,7 +49,7 @@ def findCheckmates(depth, node):
             elif depth > 1 and board.legal_moves:
                 x = Node(node, myMove.uci())
                 canCheckmate = True
-                for enemyMove in board.legal_moves:                        
+                for enemyMove in board.legal_moves:                   
                     board.push(enemyMove)
                     y = Node(x, enemyMove.uci())
                     if findCheckmates(depth - 1, y):
@@ -65,15 +64,71 @@ def findCheckmates(depth, node):
                 board.pop()
             else:
                 board.pop()
+    return returnVal
+
+def findCheckmatesVerb(depth, node, verb):
+    moves = board.legal_moves
+    returnVal = False
+    if moves:
+        if verb:
+            t = time.time()
+            c = board.legal_moves.count()
+            cl = 'w' if color else 'b' 
+            k = 1
+            o = d-depth+1
+        for myMove in moves:
             if verb:
-                print(f'{d-depth+1}: {k}/{c} took {time.time()-t} seconds')
+                t = time.time()
+            board.push(myMove)
+            if board.is_checkmate():
+                Node(node, myMove.uci())
+                board.pop()
+                returnVal = True
+            elif depth > 1 and board.legal_moves:
+                x = Node(node, myMove.uci())
+                canCheckmate = True
+                if verb:
+                    ec = board.legal_moves.count()
+                    ecl = 'b' if color else 'w' 
+                    ek = 1   
+                for enemyMove in board.legal_moves:
+                    if verb:
+                        et = time.time()                     
+                    board.push(enemyMove)
+                    y = Node(x, enemyMove.uci())
+                    if findCheckmatesVerb(depth - 1, y, int(opt['Verbose']['Depth']) > d-depth+1):
+                        board.pop()
+                    else:
+                        canCheckmate = False
+                        x.kill()
+                        board.pop()
+                        break
+                    if verb:
+                        print(f'{o*" "}{o}{ecl}: {ek}/{ec} took {round(time.time()-et,3)} seconds')
+                        ek += 1
+                if canCheckmate:
+                    returnVal = True
+                board.pop()
+            else:
+                board.pop()
+            if verb:
+                print(f'{d-depth+1}{cl}: {k}/{c} took {round(time.time()-t,3)} seconds')
                 k += 1
     return returnVal
 
+board = chess.Board(input("Enter FEN code: "))
+d = int(input("Enter search depth: "))
+color = board.turn #WHITE == True; BLACK = False
 
-if board.is_checkmate():
-    print("You are in a checkmate")
+head = Node()
+
+if board.legal_moves.count() == 0:
+    print("This position has no valid moves")
+
 t = time.time()
-findCheckmates(d, head)
+if int(opt['Verbose']['Enabled']):
+    findCheckmatesVerb(d, head, int(opt['Verbose']['Depth']))
+else:
+    findCheckmates(d, head)
 head.print()
-print(time.time()-t, 'seconds')
+print(f'The program took: {round(time.time()-t,3)} seconds')
